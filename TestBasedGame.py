791fc2d097b7f7860ebc9915bc463a96a9e6eff0
Shinaola Agbede <ashinaola@pop-os.localdomain>
2024-04-22 21:48:22 -0700

    Code Refactored. TODO: debug functions

--- TestBasedGame.py
+++ TestBasedGame.py
@@ -5,6 +5,40 @@ import helperfunctions
 import random
 import time
 
+def scan_room(player_room, item_placement, ROOMS):
+    print("You are in: {}".format(player_room))
+    print('Listing items...')
+    time.sleep(2)
+    if player_room in item_placement.values():
+        print(helperfunctions.list_items(player_room, ROOMS))
+        helperfunctions.item_menu()
+
+def check_backpack(backpack):
+    print('Checking backpack from items...')
+    time.sleep(1)
+    print(helperfunctions.check_backpack(backpack))
+
+def scan_exit(player_room, exit_location):
+    print("You are in: {}".format(player_room))
+    print("Scanning room...")
+    time.sleep(1)
+    if helperfunctions.find_exit(player_room, exit_location):
+        helperfunctions.exit_menu()
+        user_decision = int(input())
+        if user_decision == 1:
+            print('You Escaped the Compund! You win!')
+        else:
+            print('You decided against leaving.')
+
+def move_rooms(glork_room, item_placement, ROOMS):
+    helperfunctions.shuffle_glork(glork_room, ROOMS)
+    helperfunctions.shuffle_items(list(item_placement.values()))
+    print('Showing connected rooms...')
+    print(helperfunctions.show_adj_rooms(player_room, ROOMS))
+    while user_decision not in helperfunctions.show_adj_rooms(player_room, ROOMS):
+        user_decision = input('Moving rooms...Please select room:')
+    player_room = user_decision
+
 # main method
 def main():
     # set up the board
@@ -63,46 +97,19 @@ def main():
             # branching based on user decision
             match user_decision:
                 case 1: # option -> scan room
-                    print("You are in: {}".format(player_room))
-                    print('Listing items...')
-                    time.sleep(2)
-                    if player_room in item_placement.values():
-                        print(helperfunctions.list_items(player_room, ROOMS))
-                        helperfunctions.item_menu()
+                    scan_room(player_room, item_placement, ROOMS)
 
                 case 2: # option -> check backpack
-                    print('Checking backpack from items...')
-                    time.sleep(1)
-                    print(helperfunctions.check_backpack(backpack))
+                    check_backpack(backpack)
 
                 case 3: # option -> scan exit
-                    print("You are in: {}".format(player_room))
-                    print("Scanning room...")
-                    time.sleep(1)
-                    if helperfunctions.find_exit(player_room, exit_location):
-                        helperfunctions.exit_menu()
-                        user_decision = int(input())
-                        if user_decision == 1:
-                            print('You Escaped the Compund! You win!')
-                        else:
-                            print('You decided against leaving.')
+                    scan_exit(player_room, exit_location)
 
                 case 4: # option -> check current location
                     print('You are in: {}'.format(player_room))
 
                 case 5: # option -> move rooms
-                    # shuffle the glork
-                    helperfunctions.shuffle_glork(glork_room, ROOMS)
-                    # shuffle the items
-                    helperfunctions.shuffle_items(list(item_placement.values()))
-                    # show the adjacent rooms
-                    print('Showing connected rooms...')
-                    print(helperfunctions.show_adj_rooms(player_room, ROOMS))
-                    # take in user input
-                    while user_decision not in helperfunctions.show_adj_rooms(player_room, ROOMS):
-                        user_decision = input('Moving rooms...Please select room:')
-                    # set player room to selected
-                    player_room = user_decision
+                    move_rooms(glork_room, item_placement, ROOMS)
 
 if __name__ == "__main__":
     main() 
